3fe41210c8340bd3156ffad57d9ca901dde0d7f0
Matheus Batista <matheusbatistarodrigues312@gmail.com>
2022-08-22 21:36:24 -0300

    second commit

--- index.py
+++ index.py
@@ -4,6 +4,6 @@ print("\n---------- Conversor de número para extenso ----------")
 while True:
     num = input("Digite um numero de 1 a 999 -> ") # recebe o numero do usuario
     if num == '' or not num.isnumeric() or int(num) > 999 or int(num) < 1:  # verifica se o numero é válido
-        print('Esse valor não é aceito, tente colocar um número de 1 a 1000!')
+        print('Esse valor não é aceito, tente colocar um número de 1 a 999!')
     else:
         print("\n" + c.converte(str(num)) + "\n")
